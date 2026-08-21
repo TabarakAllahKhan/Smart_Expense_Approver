cf49878108b05d6d3da197e42de65010183c86f1
tabarak allah <tabarakalizai861@gmail.com>
2026-08-21 15:41:50 +0500

    added render url on frontend

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -1,4 +1,4 @@
-const BASE_API_URL="http://localhost:3001/api";
+const BASE_API_URL=import.meta.env.VITE_API_BASE_URL || "http://localhost:3001/api";
 
 type ApiClientOptions={
     method?:"GET" |  "POST" | "PATCH" | "DELETE";
